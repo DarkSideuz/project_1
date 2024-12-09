d368fd3f69b4cbbbafe31ea32633ae4bc2f8cb8e
DarkSide <darksideuz01@gmail.com>
2024-12-09 18:47:44 +0500

    full code

--- product_category/urls.py
+++ product_category/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 
-from .views import index, posts_by_category, post_detail
+from .views import home, detail
 
 urlpatterns = [
-    path('', index, name='home'),
-    path('category/<int:category_id>/', posts_by_category, name='posts_by_category'),
-    path('posts/<int:pk>/', post_detail, name='detail'),
+    path('', home, name='home'),
+    path('detail/<int:pk>/', detail, name='detail'),
 ]
--- product_category/views.py
+++ product_category/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+from . import models
+
 # Create your views here.
+
+def home(request):
+    return render(request, 'index.html')
+
+def detail(request, pk):
+    product = models.Product.objects.get(pk=pk)
+    return render(request, 'detail.html', {'product': product})
